fb1a896d9fb4f16767ab6aa8b920f9e3c48665af
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-11-30 17:15:58 -0300

    Terminado projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import TextAnimation from './components/TextAnimation'
+
+function App() {
+  return (
+    <TextAnimation/>
+  );
+}
+
+export default App;
---
+++ src/components/TextAnimation.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './TextAnimation.css'
+
+export default function(){
+    return (
+    <div className="container">
+      <div className="wavy">
+        <span>I</span>
+        <span>-</span>
+        <span>L</span>
+        <span>O</span>
+        <span>V</span>
+        <span>E</span>
+        <span>-</span>
+        <span>T</span>
+        <span>O</span>
+        <span>-</span>
+        <span>S</span>
+        <span>E</span>
+        <span>E</span>
+        <span>-</span>
+        <span>P</span>
+        <span>E</span>
+        <span>O</span>
+        <span>P</span>
+        <span>L</span>
+        <span>E</span>
+        <span>-</span>
+        <span>D</span>
+        <span>Y</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+      </div>
+    </div>
+    )
+}
